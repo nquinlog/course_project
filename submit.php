5587047cb4b3bf8d8d33072bdf63c0c886e218e2
nquinlog <nels.quinlog@smail.rasmussen.edu>
2016-04-27 21:38:13 -0400

    failing to get php to work

--- submit.php
+++ submit.php
@@ -1,3 +1,28 @@
+<!-- submit.php -->
+<html>
 <?php
+	if (isset($_POST['name']))
+	{
+		$name = $_POST['name'];
+		$base = $_POST['base'];
+		$nuts = $_POST['checknuts'];
+		$sprinkles = $_POST['checksprinkles'];
+		$cookiechunks = $_POST['checkcookie'];
+		$fruit = $_POST['checkfruit'];
+		
+		echo "<p>The data was:</p>" .
+			"<ul>" .
+			"<li>name = $name</li>" .
+			"<li>base = $base</li>" .
+			"<li>checks = $nuts </li>" .
+			"<li>$sprinkles </li>" .
+			"<li>$cookiechunks </li>" .
+			"<li>$fruit </li>" .
+			"</ul>";
+	} else {
+		echo "nothing stored";
+	}
+	
 	
 ?>
+</html>
